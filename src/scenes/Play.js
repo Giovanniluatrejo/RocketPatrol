929b6af8b5a26ed36b46d51eb9f2d1c8467f27ed
Giovanniluatrejo <gluatrej@ucsc.edu>
2020-04-21 19:25:29 -0700

    Rocket

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -23,7 +23,7 @@ class Play extends Phaser.Scene {
         // puts in green UI background
         this.add.rectangle(37, 42, 566, 64, 0x00FF00).setOrigin(0, 0);
 
-        // adds rocket (1)
+        // adds rocket (p1)
         this.p1Rocket = new Rocket(this, game.config.width/2 - 8, 431, 'rocket').setScale(0.5, 0.5).setOrigin(0, 0);
 
         // add spaceships (x3)
