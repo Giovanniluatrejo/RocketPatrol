c391127281970735f7c6698626c2042b77c2d2fe
Giovanniluatrejo <gluatrej@ucsc.edu>
2020-06-07 00:49:29 -0700

    Rocket

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -75,18 +75,6 @@ class Play extends Phaser.Scene {
             this.gameOver = true;
         
         }, null, this);
-
-        {
-            //display clock
-        
-            console.log('create');
-            this.initialTime = game.settings.gameTimer/1000;
-        
-            text = this.add.text(32, 32, 'Time: ' + formatTime(this.initialTime));
-        
-            timedEvent = this.time.addEvent({ delay: 1000, callback: onEvent, callbackScope: this, loop: true});
-        }
-        
   
     }
 
