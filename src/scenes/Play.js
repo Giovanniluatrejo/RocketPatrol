edf0ea636798c8b27755c6fcfab4a4987ffc2255
Giovanniluatrejo <gluatrej@ucsc.edu>
2020-06-06 20:20:26 -0700

    Rocket

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -7,6 +7,7 @@ class Play extends Phaser.Scene {
         this.load.image('rocket', './assets/rocket.png');
         this.load.image('spaceship', './assets/spaceship.png');
         this.load.image('starfield', './assets/starfield.png');
+
         // load spritesheet
         this.load.spritesheet('explosion', './assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
     }
@@ -17,7 +18,7 @@ class Play extends Phaser.Scene {
 
         let BGM = this.sound.add('BGM');
 
-        BGM.play( {
+        BGM.play ({
             volume: .1,
             loop: true
         })
